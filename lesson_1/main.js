9f9973ee661261a7e7e641f1a83318a6b6546d51
HP_Nam <namphg1412@gmail.com>
2024-09-30 10:40:34 +0700

    first push

---
+++ lesson_1/main.js
@@ -0,0 +1,13 @@
+console.log(!!1) // true
+console.log(!!'f8') // true
+console.log(!!['Mercedes']) // true
+
+console.log(!!false) // false
+console.log(!!0) // false
+console.log(!!'') // false
+console.log(!!null) // false
+console.log(!!undefined) // false
+console.log(!!NaN) // false
+
+console.log("hello")
+console.log(!!document.all);
