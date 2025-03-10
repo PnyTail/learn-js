2b2ec809a474a65d635d531cf5aee57ac754c8cb
HP_Nam <namphg1412@gmail.com>
2025-03-10 20:49:24 +0700

    get elements methods (2)

--- F8/HTML_DOM/main.js
+++ F8/HTML_DOM/main.js
@@ -6,20 +6,15 @@
 //     element: headingNode
 // });
 
-// let headingNodes = document.getElementsByClassName('heading');
-// let headingNodes = document.getElementsByTagName('h1');
-// console.log(headingNodes);
+// let listItems = document.querySelectorAll('.box-1 li');
+// console.log(listItems);
 
-// let headingNode = document.querySelector('.box .heading-2');
-// let headingNode = document.querySelector('.box .heading-2:first-child');
-// let headingNode = document.querySelectorAll('.box .heading-2');
-// console.log(headingNode);
+let boxNode = document.querySelector('.box-1');
+// công việc 1: sử dụng tới `boxNode`
+// ...
+console.log(boxNode);
 
-// console.log(document.forms); //lấy ra tất cả các form trong trang
-// console.log(document.forms['form-1']); //lấy ra form có id là form-1
-console.log(document.forms.formTest);
+// công việc 2: sử dụng tới li elements là con của `.box-1`
+console.log(boxNode.querySelectorAll('li'));
 
-// console.log(document.images); //lấy ra tất cả các ảnh trong trang
-// console.log(document.scripts); //lấy ra tất cả các script trong trang
-// console.log(document.links); //lấy ra tất cả các link trong trang
 
