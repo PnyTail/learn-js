b4551a5e8210516a880f367d0250bef0d03fe92f
HP_Nam <namphg1412@gmail.com>
2025-03-10 20:39:49 +0700

    get element methods

---
+++ F8/HTML_DOM/main.js
@@ -0,0 +1,25 @@
+
+
+// let headingNode = document.getElementById('heading');
+// console.log(headingNode);
+// console.log({
+//     element: headingNode
+// });
+
+// let headingNodes = document.getElementsByClassName('heading');
+// let headingNodes = document.getElementsByTagName('h1');
+// console.log(headingNodes);
+
+// let headingNode = document.querySelector('.box .heading-2');
+// let headingNode = document.querySelector('.box .heading-2:first-child');
+// let headingNode = document.querySelectorAll('.box .heading-2');
+// console.log(headingNode);
+
+// console.log(document.forms); //lấy ra tất cả các form trong trang
+// console.log(document.forms['form-1']); //lấy ra form có id là form-1
+console.log(document.forms.formTest);
+
+// console.log(document.images); //lấy ra tất cả các ảnh trong trang
+// console.log(document.scripts); //lấy ra tất cả các script trong trang
+// console.log(document.links); //lấy ra tất cả các link trong trang
+
---
+++ F8/HTML_DOM/overview.js
@@ -0,0 +1,15 @@
+
+// HTML DOM
+ 
+// có 3 thành phần
+
+/**
+ * 1. Element: là các thẻ HTML. ID, class, tag, css selector, html collection
+ * 2. Attribute: là các thuộc tính của thẻ HTML
+ * 3. Text: là nội dung của thẻ HTML
+ */
+
+// Node: là tất cả các thành phần của HTML DOM (Element, Attribute, Text)
+
+
+// browser: html -> DOM -> DOM API (web api)
