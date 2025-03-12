55c6051d50396408a0ecd56d0ed39e0930e1f96b
HP_Nam <namphg1412@gmail.com>
2025-03-13 00:17:53 +0700

    DOM events: input, select, onchange, keyup, keydown

--- F8/HTML_DOM/main.js
+++ F8/HTML_DOM/main.js
@@ -1,24 +1,74 @@
 /**
  * DOM events
- * 1. Attribute events
- * 2. Assign event using the element node
+ * 
+ * 1. input / select
+ * 2. keyup / keydown
  */
 
-let h1Elements = document.querySelectorAll('h1');
+let inputValue;
 
-// event or "e" for short
-// h1Elements.onclick = function(e) {
-//     // console.log(Math.floor(Math.random() * 100));
-//     console.log(e.target);
+// let inputElement = document.querySelector('input[type="text"]');
+
+// chỉ hiện giá trị khi blur (không focus vào ô input) ra khỏi input
+// inputElement.onchange = function(e) {
+//     console.log(e.target.value);
 // }
 
-for (let i = 0; i < h1Elements.length; i++) {
-    h1Elements[i].onclick = function(e) {
-        console.log(e.target);
-    }
+// hiện giá trị ngay khi nhập vào input
+// inputElement.oninput = function(e) {
+//     // console.log(e.target.value);
+//     inputValue = e.target.value;
+// }
+/**------------------------------------------------ */
+
+// let inputElement = document.querySelector('input[type="checkbox"]');
+
+// inputElement.onchange = function(e) {
+//     console.log(e.target.checked);
+// }
+
+/**------------------------------------------------ */
+
+// let inputElement = document.querySelector('select');
+
+// inputElement.onchange = function(e) {
+//     console.log(e.target.value);
+// }
+
+/**------------------------------------------------ */
+/**
+ * 2. keyup / keydown
+ */
+
+// let inputElement = document.querySelector('input[type="text"]');
+
+// inputElement.onkeyup = function(e) {
+//     console.log(e.which);
+
+//     switch(e.which) {
+//         case 27:
+//             console.log('Exit');
+//             break;
+//         case 13:
+//             console.log('Enter');
+//             break;
+//     }
+// }
+
+/**
+ * which, keyCode, charCode (deprecated) - không nên sử dụng
+ */
+
+// lắng nghe sự kiện keydown trên toàn bộ trang web
+document.onkeydown = function(e) {
+    // console.log(e.code);
+    console.log(e.key);
+    // switch(e.code) {
+    //     case 27:
+    //         console.log('Exit');
+    //         break;
+    //     case 13:
+    //         console.log('Enter');
+    //         break;
+    // }
 }
-
-
-
-
-
