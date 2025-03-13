60f4d877d72203e5a9b4908cd2dd2f22767b6aab
HP_Nam <namphg1412@gmail.com>
2025-03-13 17:48:03 +0700

    JSON definition

---
+++ F8/JSON, Fetch, Postman/app.js
@@ -0,0 +1,21 @@
+/**
+ * JSON
+ * 
+ * 1. là 1 định dạng dữ liệu (chuỗi)
+ * 2. JavaScript Object Notation
+ * 3. JSON: Number, String, Boolean, Array, Object, null
+ * 
+ * stringify: từ javascript type -> JSON / parse: từ JSON -> javascript type
+ */
+
+// let json = '["JavaScript", "PHP"]';
+// let json = '{"name": "Nam", "age": 18}';
+// console.log(JSON.parse(json));
+
+// let a = '1';
+// let a = 'true';
+// let a = 'null';
+// let a = '"abc"';
+// console.log(JSON.parse(a));
+
+console.log(JSON.stringify([1, 2, 3]));
