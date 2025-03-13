04e71225b777c05eddfb501d01b896e4b911a5ce
HP_Nam <namphg1412@gmail.com>
2025-03-13 22:18:21 +0700

    promise

--- F8/JSON, Fetch, Postman/app.js
+++ F8/JSON, Fetch, Postman/app.js
@@ -1,21 +1,87 @@
 /**
- * JSON
+ * Promise
  * 
- * 1. là 1 định dạng dữ liệu (chuỗi)
- * 2. JavaScript Object Notation
- * 3. JSON: Number, String, Boolean, Array, Object, null
- * 
- * stringify: từ javascript type -> JSON / parse: từ JSON -> javascript type
+ * - sync
+ * - async
  */
 
-// let json = '["JavaScript", "PHP"]';
-// let json = '{"name": "Nam", "age": 18}';
-// console.log(JSON.parse(json));
+/**
+ * async: làm việc bất đồng bộ
+ * setTimeout, setInterval, fetch, XMLHttpRequest, file reading (đọc file), request animation frame
+ * 
+ * sync: làm việc đồng bộ
+ * DOM, console.log, math, array, string, object, ...
+ */
 
-// let a = '1';
-// let a = 'true';
-// let a = 'null';
-// let a = '"abc"';
-// console.log(JSON.parse(a));
+// setTimeout(function() {
+//     console.log('1')
+// }, 1000);
 
-console.log(JSON.stringify([1, 2, 3]));
+// console.log('2');
+
+/**
+ * callback hell
+ * - callback hell là tình trạng lồng callback quá nhiều
+ * - code trở nên khó đọc, khó hiểu, khó maintain
+ * - callback hell xảy ra khi có quá nhiều callback lồng vào nhau
+ * - giải pháp: Promise, async/await
+ * 
+ * pyramid of doom
+ * - pyramid of doom là tình trạng lồng callback quá nhiều
+ * - code trở nên khó đọc, khó hiểu, khó maintain
+ * - giải pháp: Promise, async/await
+ * 
+ * Promise
+ * - Promise is an object that may produce a single value some time in the future
+ * - Promise is a placeholder for a value that may not be available yet
+ * - Promise is a proxy for a value not necessarily known when the promise is created
+ * - Promise is a way to write async code that is easier to read and maintain
+ * 
+ */
+
+/**
+ * tạo promise
+ * 1. new Promise
+ * 2. excutor
+ */
+
+/**
+ * 3 trạng thái của promise
+ * - pending: trạng thái khởi tạo, chờ xử lý
+ * - fulfilled: trạng thái thành công
+ * - rejected: trạng thái thất bại
+ * 
+ * 1 promise chỉ chuyển từ pending -> fulfilled hoặc pending -> rejected
+ * 
+ * promise.then() -> xử lý khi promise thành công
+ * promise.catch() -> xử lý khi promise thất bại
+ * promise.finally() -> xử lý sau khi promise kết thúc (thành công hoặc thất bại)
+ */
+
+let promise = new Promise(
+    // excutor
+    function(resolve, reject) {
+        // Logic
+        // Khi logic Thành công: resolve()
+        // Khi logic Thất bại: reject()
+
+        resolve([{
+            id: 1,
+            name: 'Javascript'
+        }]);
+
+        // reject('có lỗi xảy ra');
+    }
+);
+
+promise
+    .then(function(courses) {
+        console.log(courses);
+    })
+    .catch(function(error) {
+        console.log(error);
+    })
+    .finally(function() {
+        console.log('Done');
+    });
+
