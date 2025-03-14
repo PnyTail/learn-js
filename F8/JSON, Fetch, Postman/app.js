89e9bde675be49f17474ea388e50c9ab9b910735
HP_Nam <namphg1412@gmail.com>
2025-03-14 17:10:43 +0700

    install json server

--- F8/JSON, Fetch, Postman/app.js
+++ F8/JSON, Fetch, Postman/app.js
@@ -1,35 +1,20 @@
 /**
- * Fetch API
- * 
- * front-end
- * back-end
- * 
- * API (url) - Application Programming Interface
- * cổng giao tiếp giữa các phần mềm
- * 
- * backend -> API -> fetch -> json -> json.parse -> javascript types
- * -> render ra giao diện với html
+ * cài đặt json-server : https://github.com/typicode/json-server
+ * cài đặt json-server : 
+ *  npm init
+ *  npm install json-server
+ *  tạo file db.json
+ *  thêm dòng này vào file package.json, đoạn scripts:
+ *      "start": "json-server --watch db.json",
+ *  npm start 
  */
 
-let postApi = 'https://jsonplaceholder.typicode.com/posts';
+let courseApi = 'http://localhost:3000/courses';
 
-fetch(postApi)
+fetch(courseApi)
     .then(function(response) {
-        // JSON.parse: chuyển từ json -> javascript types
-        return response.json(); //json() nó đã parse cho mình rồi
+        return response.json();
     })
-    .then(function(posts) {
-        // console.log(posts);
-        let htmls = posts.map(function(post) {
-            return `<li>
-                <h2>${post.title}</h2>
-                <p>${post.body}</p>        
-            </li>`;
-        });
-
-        let html = htmls.join('');
-        document.getElementById('post-block').innerHTML = html;
-    })
-    .catch(function(err) {
-        console.log(`Có lỗi: ${err}`);
+    .then(function(courses) {
+        console.log(courses);
     })
