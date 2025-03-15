6ca8a0cab8d09de4dd0c42dff8b67d382304a947
HP_Nam <namphg1412@gmail.com>
2025-03-15 19:53:26 +0700

    ES6 intro

---
+++ F8/ES6/app.js
@@ -0,0 +1,19 @@
+/**
+ * ECMAScript 6 - ES6 - ECMAScript 2015
+ * 
+ * 1. let, const
+ * 2. Template literals
+ * 3. multi-line strings
+ * 4. Arrow functions
+ * 5. classes
+ * 6. default parameters values
+ * 7. destructuring
+ * 8. rest parameters
+ * 9. Spread
+ * 10. Enhanced Object Literals
+ * 11. Tagged template literals
+ * 12. Modules
+ * 
+ * 
+ * Babel
+ */
