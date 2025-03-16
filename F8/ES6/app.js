8747b4e54991212640ca409bcfb4e98f28bfcf1c
HP_Nam <namphg1412@gmail.com>
2025-03-16 14:20:25 +0700

    Enhanced Object Literals

--- F8/ES6/app.js
+++ F8/ES6/app.js
@@ -18,19 +18,50 @@
  * Babel
  */
 
-// function logger(log = 'giá trị mặc định') {
-//     console.log(log);
+// Enhanced Object Literals
+
+/**
+ * 1. Property value shorthand
+ * 2. Method properties
+ * 3. Computed property names
+ * 
+ * 1. Định nghĩa key:value cho object (ngắn gọn hơn)
+ * 2. Định nghĩa method cho object (ngắn gọn hơn)
+ * 3. Định nghĩa key của object dưới dạng biến
+ */
+
+let name = 'JavaScript';
+let price = 1000;
+
+// Cách cũ
+// let course = {
+//     name: name,
+//     price: price,
+//     getName: function() {
+//         return name;
+//     }
+// };
+
+// Cách mới: enhanced object literals
+// let course = {
+//     name,
+//     price,
+//     getName() {
+//         return name;
+//     }
 // }
 
-// function logger(log, isAlert) {
-//     if (isAlert) return alert(log);
-//     console.log(log);
-// }
+// // console.log(course);
+// console.log(course.getName());
 
-function logger(log, type = 'log') {
-    console[type](log);
-}
 
-// logger('message');
-logger('message', 'warn');
-// logger('message', 'error');
+// 3. Định nghĩa key của object dưới dạng biến
+let fieldName = 'name';
+let fieldPrice = 'price';
+
+const course = {
+    [fieldName]: 'JavaScript',
+    [fieldPrice]: 1000
+};
+
+console.log(course);
