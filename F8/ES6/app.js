b600fb232af0c486dc90a75eba6c5fd140cb6457
HP_Nam <namphg1412@gmail.com>
2025-03-16 14:53:46 +0700

    bổ sung về rest, destructuring

--- F8/ES6/app.js
+++ F8/ES6/app.js
@@ -18,57 +18,29 @@
  * Babel
  */
 
-// destructuring: phân rã, dùng với object và array
+// rest parameters là phần còn lại của 1 list các tham số
+// rest parameters sẽ trả về 1 array
+// function logger(a, ...params) {
+//     console.log(params);
+// }
+// logger(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-let array = ['Javascript', 'PHP', 'Ruby'];
+// function logger({  name, price, ...rest }) {
+//     console.log(name);
+//     console.log(price);
+//     console.log(rest);
+// }
 
-// cách cũ
-// let a = array[0];
-// let b = array[1];
-// let c = array[2];
-
-// cách mới: lấy ra 3 phần tử trong mảng array
-// let [a, b, c] = array;
-// console.log(a, b, c);
-
-// lấy ra 2 phần tử trong mảng array
-// let [a, , c] = array;
-// console.log(a, c);
-
-// lấy ra phần tử đầu tiên và phần còn lại (rest parameters)
-// let [a, ...rest] = array;
-
-// console.log(a);
-// console.log(rest);
-
-/** ------------------------------- */
-
-let course = {
-    name: 'Javascript',
-    price: 1000,
-    image: 'image-address'
-    // ,children: {
-    //     name: 'ReactJS'
-    // }
-};
-
-// let {name, price} = course;
-// let {name, ...rest} = course;
-// let { name: parentName, children: { name: childrenName }} = course;
-
-// console.log(name);
-// console.log(rest);
-// console.log(parentName);
-// console.log(childrenName);
-
-// let { name, description = 'default description' } = course;
-// console.log(name);
-// console.log(description);
-
-// dấu 3 chấm -> toán tử rest
-function logger(...params) {
-    console.log(params);
+function logger([a, b, ...rest]) {
+    console.log(a);
+    console.log(b);
+    console.log(rest);
 }
 
-// logger(1, 2, 3, 4, 5);
-console.log(logger(1, 2, 3, 4, 5)); // undefined
+// logger({
+//     name: 'Javascript',
+//     price: 1000,
+//     description: 'Good'
+// })
+
+logger([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
