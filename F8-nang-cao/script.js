fa7a1eb248e3bd133aae9af35bdead7fd1d5eb98
HP_Nam <namphg1412@gmail.com>
2025-03-18 23:46:49 +0700

    call method

--- F8-nang-cao/script.js
+++ F8-nang-cao/script.js
@@ -1,15 +1,29 @@
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
-
-console.log($('#heading'));
-
 /**
- * delegate pattern js
+ * Call method
+ * 
+ * Là phương thức trong prototype của Function destructor, phương thức này được dùng để
+ *  gọi hàm và cũng có thể bind this cho hàm
+ * 
+ * Ví dụ:
+ *  - Gọi hàm với call method
+ *  - Gọi hàm và bind this, lưu ý trong strict mode vẫn có this nếu được bind
+ *  - Thể hiện tính kế thừa (extends) trong OOP
+ *  - Mượn hàm (function borrowing), thêm ví dụ với arguments
  */
 
-/**
- * Phương thức bind() cho phép ràng buộc this cho một phương thức (function)
- * Phương thức bind() sẽ trả về một hàm mới với context được bind
- * Hàm được trả về từ bind() vẫn có thể nhận các đối số của hàm gốc
- */
+const teacher = {
+    firstName: 'Minh',
+    lastName: 'Thu'
+}
 
+const me = {
+    firstName: 'Nam',
+    lastName: 'Hoàng',
+    showFullName() {
+        // console.log(this)
+        console.log(`${this.firstName} ${this.lastName}`);
+    }
+}
+
+// me.showFullName(); // Nam Hoàng
+me.showFullName.call(teacher); // Minh Thu, mượn hàm showFullName của me cho teacher
