8ccc5a7c648c0597a8f961c2780d0a0bc6b05ddc
HP_Nam <namphg1412@gmail.com>
2025-03-18 13:43:58 +0700

    strict mode

--- F8-nang-cao/script.js
+++ F8-nang-cao/script.js
@@ -1,28 +1,44 @@
-/**
- * 1. IIFE
- * 2. Closure
- * 3. Hoisting
- * 4. Strict mode
- * 5. This keyword
- * 6. Bind method
- * 7. Call method
- * 8. Apply method
- * 9. Catching, throwing errors
- * 10. Promise
- * 11. Async function, await
- */
+"use strict";
 
-/** closure */
+// fullName = "Nguyen Van A";
+
+// function testFunc() {
+//     age = 18; // tạo biến age ở phạm vi global
+// }
+
+// testFunc();
+// console.log(fullName); // Nguyen Van A (không dùng strict mode sẽ hiển thị ra giá trị của biến)
+// console.log(age); // 18
 
 /**
- * Closure là một hàm có thể ghi nhớ nơi nó được tạo và truy cập đến biến ở phạm vi ngoài
- * 
- * Ứng dụng:
- *      - viết code ngắn hơn
- *      - biểu diễn, ứng dụng tính private data trong OOP
- * 
- * Lưu ý:
- *      - Biến được tham chiếu (refer) trong closure sẽ không được xoá khỏi bộ nhớ khi hàm cha thực thi xong
- *      - Các khái niệm Javascript nâng cao dễ gây nhầm lẫn
+ * object.freeze() sẽ đóng băng object, không cho phép thay đổi giá trị của object
+ * object.seal() sẽ đóng băng object, không cho thêm hoặc xóa thuộc tính của object
+ * object.defineProperty() sẽ định nghĩa thuộc tính cho object
  */
 
+// const student= {} //nếu dùng kiểu khai báo object như bình thường thì writable mặc định là true
+
+// Object.defineProperty(student, 'name', {
+//     value: 'Nguyen Van A',
+//     writable: false, // không cho phép thay đổi giá trị, mặc định là false khi dùng defineProperty
+// })
+
+/**
+ * báo lỗi khi hàm có tham số trùng tên
+ * 
+ * function sum(a, a) {
+ *  return a + a
+ * }
+ * console.log(sum(1, 2)) // 4 (lấy số sau ghi đè số trước)
+ * 
+ */
+
+/**
+ * Khai báo hàm trong block code thì hàm sẽ thuộc phạm vi block code đó
+ * Không đặt tên biến, tên hàm bằng một số từ khoá "nhạy cảm" của ngôn ngữ
+ * 
+ * Công dung của strict mode:
+ *      - Tránh "quên" từ khoá khi khai báo biến (var let const)
+ *      - Tránh trùng tên biến dẫn tới lỗi logic
+ *      - Sử dụng bộ nhớ hiệu quả hơn vì không tạo biến global
+ */
